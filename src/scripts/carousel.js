88e51d0671edf02096a62d67f645605cba7f528f
LeoVS09 <LeoVS010@gmail.com>
2017-04-25 01:51:36 +0300

    Complete base of carousel

--- src/scripts/carousel.js
+++ src/scripts/carousel.js
@@ -1,116 +1,118 @@
 import './carousel.styl'
 
-const carouselClass = 'carousel'
-const carouselTrackClass = 'carousel-track'
-const carouselSlideClass = 'carousel-slide'
-const carouselSlideActiveClass = 'carousel-slide-active'
-const transitionDuration = 1000
 
 
+class Carousel{
+    carouselClass = 'carousel'
+    carouselTrackClass = 'carousel-track'
+    carouselSlideClass = 'carousel-slide'
+    carouselSlideActiveClass = 'carousel-slide-active'
+    transitionDuration = 1000
+    transitionDelay = 2000
 
-export default ({container, endless = true}) => {
-    if(!container) throw new Error('Carousel need container')
-    let {currentSlide, width, track, slides} = injectNodes(container, endless),
-        currentOffset = 0,
-        stepTimer = setInterval(function () {
-            let currentState = makeStep({currentSlide,currentOffset, width, track, slides, endless})
-            currentSlide = currentState.currentSlide
-            currentOffset = currentState.currentOffset
-            let direction = 1
-            setTimeout(function () {
-                relocateSlide({track,slides}, direction === 1)
-                currentSlide -= direction
-                let currentState = returnTrack({track, width, currentOffset, currentSlide})
-                currentSlide = currentState.currentSlide
-                currentOffset = currentState.currentOffset
-            },transitionDuration)
-            if(currentSlide == slides.length-1)
-                clearInterval(stepTimer)
-        },2000)
-}
+    constructor({container, endless = true}){
+        if(!container) throw new Error('Carousel need container')
+        this.container = container
+        this.endless = endless
+        this.injectNodes()
+        this.currentOffset = 0
+        this.startStep()
+    }
 
-let injectNodes = (container,endless) => {
-    let currentSlide = 0
-    let width = container.offsetWidth
-    let slides = Array.prototype.slice.call(container.childNodes)
-        .map(wrapToSlide(width))
-    remakeSlides(slides,endless)
-    container.classList.add(carouselClass)
-    slides[currentSlide].classList.add(carouselSlideActiveClass)
-    let track = document.createElement('div')
-    track.classList.add(carouselTrackClass)
-    track.style.transitionDuration = transitionDuration + 'ms'
-    track.innerHTML = slides.map(el => el.outerHTML).join('')
-    container.innerHTML = track.outerHTML
-    track = container.querySelector('.' + carouselTrackClass)
-    slides = Array.prototype.slice.call(track.querySelectorAll('.' + carouselSlideClass))
-    let trackWidth = calcTrackWidth(width, slides.length, endless)
-    track.style.width =  trackWidth + 'px';
-    return {currentSlide, width, track, slides}
-}
+    injectNodes(){
+        this.currentSlide = 0
+        this.width = this.container.offsetWidth
+        let slides = Array.prototype.slice.call(this.container.childNodes)
+            .map(this.wrapToSlide.bind(this))
+        // remakeSlides(slides,endless)
+        this.container.classList.add(this.carouselClass)
+        slides[this.currentSlide].classList.add(this.carouselSlideActiveClass)
+        let track = document.createElement('div')
+        track.classList.add(this.carouselTrackClass)
+        track.style.transitionDuration = this.transitionDuration + 'ms'
+        track.innerHTML = slides.map(el => el.outerHTML).join('')
+        this.container.innerHTML = track.outerHTML
+        track = this.container.querySelector('.' + this.carouselTrackClass)
+        this.track = track
+        slides = Array.prototype.slice.call(track.querySelectorAll('.' + this.carouselSlideClass))
+        this.slides = slides
+        let trackWidth = this.calcTrackWidth()
+        track.style.width =  trackWidth + 'px';
+    }
 
-function remakeSlides(slides,endless) {
-    if(endless) {
-        let first = slides[0].cloneNode(true)
-        slides.push(first)
+    calcTrackWidth() {
+        // if(endless)
+        //     return initial*(length+1)
+        // else
+        return this.width*this.slides.length
+    }
+
+    startStep(){
+        this.stepTimer = this.autoStep()
+    }
+
+    autoStep = () => setInterval(() => {
+        this.makeStep()
+        let direction = 1
+        setTimeout(() => {
+            this.relocateSlide(direction === 1)
+            this.currentSlide -= direction
+            this.returnTrack()
+        },this.transitionDuration)
+        if(this.currentSlide == this.slides.length-1)
+            clearInterval(this.stepTimer)
+    },this.transitionDelay)
+
+    wrapToSlide(element){
+        let slide = document.createElement('div')
+        slide.classList.add(this.carouselSlideClass)
+        slide.style.width = this.width + 'px'
+        slide.appendChild(element)
+        return slide
+    }
+
+    relocateSlide(toEnd = true) {
+        if(toEnd){
+            let first = this.slides.shift()
+            this.track.removeChild(first)
+            this.track.appendChild(first)
+            this.slides.push(first)
+        } else {
+            let last = this.slides.pop()
+            this.track.removeChild(last)
+            this.track.insertBefore(last, this.track.firstChild)
+            this.slides.unshift(last)
+        }
+    }
+
+    returnTrack() {
+        this.track.style.transitionDuration = '0ms'
+        this.currentOffset = this.currentSlide*this.width
+        this.track.style.transform = 'translateX(-' + this.currentOffset + 'px)'
+    }
+
+    makeStep(direction = 1) {
+        this.track.style.transitionDuration = this.transitionDuration + 'ms'
+        this.slides[this.currentSlide].classList.remove(this.carouselSlideActiveClass)
+        this.currentOffset += direction*this.width;
+        this.track.style.transform = 'translateX(-' + this.currentOffset + 'px)';
+        this.currentSlide += direction
+        this.slides[this.currentSlide].classList.add(this.carouselSlideActiveClass)
+    }
+
+    goTo(target) {
+        this.track.style.transitionDuration = transitionDuration + 'ms'
+        this.slides[this.currentSlide].classList.remove(this.carouselSlideActiveClass)
+        this.currentOffset = this.width*target;
+        this.track.style.transform = 'translateX(-' + this.currentOffset + 'px)';
+        this.currentSlide = target
+        this.slides[this.currentSlide].classList.add(this.carouselSlideActiveClass)
     }
 }
 
-function calcTrackWidth(initial,length,endless) {
-    // if(endless)
-    //     return initial*(length+1)
-    // else
-        return initial*length
-}
+export default options =>
+    new Carousel(options)
 
-let wrapToSlide = width => (element) => {
-    let slide = document.createElement('div')
-    slide.classList.add(carouselSlideClass)
-    slide.style.width = width + 'px'
-    slide.appendChild(element)
-    return slide
-}
 
-function makeStep({slides, currentSlide, currentOffset, track, width, endless}, direction = 1) {
-    slides[currentSlide].classList.remove(carouselSlideActiveClass)
-    currentOffset += direction*width;
-    track.style.transform = 'translateX(-' + currentOffset + 'px)';
-    currentSlide += direction
-    slides[currentSlide].classList.add(carouselSlideActiveClass)
-    return {currentOffset, currentSlide}
-}
 
-function goTo({slides, currentSlide, currentOffset, track, width}, target) {
-    slides[currentSlide].classList.remove(carouselSlideActiveClass)
-    currentOffset = width*target;
-    track.style.transform = 'translateX(-' + currentOffset + 'px)';
-    currentSlide = target
-    slides[currentSlide].classList.add(carouselSlideActiveClass)
-    return { currentSlide, currentOffset }
-}
 
-function autoStep() {
-
-}
-
-function relocateSlide({track, slides},toEnd = true) {
-    if(toEnd){
-        let first = slides.shift()
-        track.removeChild(first)
-        track.appendChild(first)
-        slides.push(first)
-    } else {
-        let last = slides.pop()
-        track.removeChild(last)
-        track.insertBefore(last, track.firstChild)
-        slides.unshift(last)
-    }
-}
-
-function returnTrack({track, width, currentOffset, currentSlide}) {
-    track.style.transitionDuration = 0
-    currentOffset = currentSlide*width
-    track.style.transform = 'translateX(-' + currentOffset + 'px)'
-    track.style.transitionDuration = transitionDuration + 'ms'
-    return {currentOffset, currentSlide}
-}
